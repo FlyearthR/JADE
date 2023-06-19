708c477da68bb7f7b052722c04736737cb63111b
Tom Rousseaux <rousseaux1.linerider@gmail.com>
2023-06-19 15:38:31 +0200

    Error fixed

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-const NB_FOLLOWER: usize = 3;
-const QNAME = "/nts_mq";
 use posixmq::PosixMq;
 use std::io::Result;
 use fork::{fork, Fork};
@@ -7,17 +5,19 @@ use nix::unistd::execve;
 use std::ffi::CStr;
 use std::ffi::CString;
 
-fn init_queues(nb: usize) -> Result<Vec<PosixMq>> {
+const NB_FOLLOWER: u8 = 3;
+const QNAME: &str = "/nts_mq";
+const EXE_NAME: &str = "./callee";
+
+fn init_queues(nb: u8) -> Result<Vec<PosixMq>> {
     let mut qs = Vec::new();
     for i in 0..nb {
-        let t = PosixMq::create(&format!("{}_{}", QNAME, i))?;
-        t.set_cloexec(false)?;
-        qs.push(t);
+        qs.push(PosixMq::create(&format!("{}_{}", QNAME, i))?);
     }
     Ok(qs)
 }
 
-fn unlink_queues(nb: usize) -> Result<()>{
+fn unlink_queues(nb: u8) -> Result<()>{
     for i in 0..nb {
         posixmq::remove_queue(&format!("{}_{}", QNAME, i))?;
     }
@@ -33,14 +33,17 @@ fn run_follower(id: u8, exe: String, args: &[&CStr], env: &[&CStr]) -> Result<i3
             execve(&CString::new(exe.as_str()).unwrap().as_c_str(), args, env);
             Ok(0)
         }
-        Err(e) => Err(e),
+        Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, String::from("Fork failed"))),
     } 
 }
 
 fn main() {
     println!("Hello, world!");
+    for i in 0..NB_FOLLOWER {
+        run_follower(i, String::from(EXE_NAME), &[], &[]).unwrap();
+    }
+
     let qs = init_queues(NB_FOLLOWER).unwrap();
-    
 
     unlink_queues(NB_FOLLOWER).unwrap();
 }
