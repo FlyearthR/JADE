08f3685d34ba921095545226f7057965a6ac7277
Tom Rousseaux <rousseaux1.linerider@gmail.com>
2025-01-15 17:44:06 +0100

    implem __read_chk

--- c_src/syscalls.c
+++ c_src/syscalls.c
@@ -454,3 +454,23 @@ ssize_t read(int fd, void *buf, size_t count)
         } while (ret == 0 && empty_fun());
         return ret;
 }
+
+ssize_t __read_chk(int fd, void *buf, size_t count)
+{
+        LIBC_FUNCTION(ssize_t, __read_chk, int fd, void *buf, size_t count);
+        if (getsockname(fd, NULL, 0) == -1 && errno == ENOTSOCK)
+        { // this is not an FD to a socket, we have to pass it to the kernel
+                LIBC_FUNCTION_GET(__read_chk)(fd, buf, count);
+        }
+        // TODO: configure socket as non blocking at opening time
+        int flags_s = fcntl(fd, F_GETFL, 0);
+        if (flags_s == -1)
+                return -1;
+
+        int ret;
+        do
+        {
+                ret = LIBC_FUNCTION_GET(__read_chk)(fd, buf, count);
+        } while (ret == 0 && empty_fun());
+        return ret;
+}
