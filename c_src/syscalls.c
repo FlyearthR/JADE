b7eac757336e7ab6b00ab8b037541d8364fa620d
Tom Rousseaux <tom.rousseaux@uclouvain.be>
2025-05-21 22:35:56 +0000

    Typo

--- c_src/syscalls.c
+++ c_src/syscalls.c
@@ -382,7 +382,7 @@ int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) // TODO:
     memcpy(&f->fi.addr, addr, addrlen);
     fd_ip_elem* elem;
     LOGS("list before prepend\n");
-    LL_FOREACH(fd_ip_list, elem
+    LL_FOREACH(fd_ip_list, elem)
     {
         LOGS("fd: %i, ip: %s\n", elem->fi.fd, inet_ntoa(((struct sockaddr_in*)&(elem->fi.addr))->sin_addr));
     }
